a924ad885b3f858f40ea7ab7a6be1714e90ba1f4
Marais Rossouw <me@maraisr.com>
2020-06-25 15:00:42 +1000

    chore: Impl things

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { RelayEnvironmentProvider } from "react-relay/hooks";
+import { Auth0Client } from "@auth0/auth0-spa-js";
+import { Environment, Network, RecordSource, Store } from "relay-runtime";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+
+const client = new Auth0Client({
+  client_id: "",
+  domain: "",
+});
+
+const fetchFunction = async (params, variables) => {
+  const token = await client.getTokenSilently();
+
+  const response = await fetch(`http://my.api.com/graphql`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      query: params.text,
+      variables,
+    }),
+  });
+
+  return response.json();
+};
+
+const source = new RecordSource();
+const store = new Store(source);
+const network = Network.create(fetchFunction);
+
+const relayModernEnvironment = new Environment({
+  network,
+  store,
+});
 
 function App() {
   return (
@@ -10,6 +47,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Auth0Provider client={client}>
+          <MyAuthBackedApp />
+        </Auth0Provider>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,5 +62,23 @@ function App() {
     </div>
   );
 }
+
+const MyAuthBackedApp = () => {
+  const x = useAuth0();
+
+  return x.isAuthenticated ? (
+    <RelayEnvironmentProvider environment={relayModernEnvironment}>
+      <RelayComponent />
+    </RelayEnvironmentProvider>
+  ) : (
+    <div>Please log in...</div>
+  );
+};
+
+const RelayComponent = () => {
+  // Here you'd put your fragments, or useQuery's
+
+  return <div />;
+};
 
 export default App;
